22520fc315081c270be086073297f60faab3a4a2
Swarna7414 <swarnasaisankar044@gmail.com>
2024-10-20 00:38:15 +0530

    added html pages

--- src/main/java/Weather/application/Controller/WeatherController.java
+++ src/main/java/Weather/application/Controller/WeatherController.java
@@ -6,16 +6,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/weather")
 public class WeatherController {
 
     private static Logger logger= LoggerFactory.getLogger(WeatherController.class);
+
+    private String realkey= "1497ada8dc73490586f125605241909";
 
     private final ServiceInterface service;
     @Autowired
@@ -24,33 +28,43 @@ public class WeatherController {
     }
 
     @GetMapping(value = "/fulldata",produces = MediaType.APPLICATION_JSON_VALUE)
-    public FullResponse getResponse(@RequestParam String key, @RequestParam String location, @RequestParam Integer days){
+    public FullResponse getResponse(@RequestParam String location, @RequestParam Integer days){
         logger.info("Entered into the full data");
-        return service.getfullRespone(key, location, days);
+        return service.getfullRespone(realkey, location, days);
     }
 
     @GetMapping(value = "/location", produces = "application/json")
-    public Location getLocation(@RequestParam String key, @RequestParam String location, @RequestParam Integer days){
+    public Location getLocation(@RequestParam String location, @RequestParam Integer days){
         logger.info("entering into the Location");
-        return service.getLocation(key, location, days);
+        return service.getLocation(realkey, location, days);
     }
 
     @GetMapping(value = "/current", produces = "application/json")
-    public Current getcurrent(@RequestParam String key, @RequestParam String location, @RequestParam Integer days){
+    public Current getcurrent(@RequestParam String location, @RequestParam Integer days){
         logger.info("entering into the current controller method");
-        return service.getCurrentWeatherDeatios(key, location, days);
+        return service.getCurrentWeatherDeatios(realkey, location, days);
     }
 
     @GetMapping(value = "/forecast", produces = "application/json")
-    public Forecast getForecasteddata(@RequestParam String key, @RequestParam String location, @RequestParam Integer days){
+    public Forecast getForecasteddata(@RequestParam String location, @RequestParam Integer days){
         logger.info("entering into the forecast method");
-        return service.getForcastedetails(key, location, days);
+        return service.getForcastedetails(realkey, location, days);
     }
 
     @GetMapping("/required")
-    public ExtractedData getExtratedData(@RequestParam String key, @RequestParam String location, @RequestParam Integer days){
-        logger.info("Entering into the required details method");
-        return service.getextracteddata(key,location,days);
+    public String getExtratedData(@RequestParam String location, @RequestParam Integer days, Model model){
+        ExtractedData data=service.getextracteddata(realkey,location,days);
+        model.addAttribute("name",data.getName());
+        model.addAttribute("country",data.getCountry());
+        model.addAttribute("localdate",data.getLocaldate());
+        model.addAttribute("localtime",data.getLocaltime());
+        model.addAttribute("weather",data.getWeather());
+        model.addAttribute("humidity",data.getHumidity());
+        model.addAttribute("tempc",data.getTempc());
+        model.addAttribute("heatindexc",data.getHeatindexc());
+        model.addAttribute("dewpointc",data.getDewpointc());
+        model.addAttribute("gust_kph",data.getGust_kph());
+        return "index";
     }
 
 
